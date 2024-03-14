7ab39faae9cd6a88ca988156d8ecf746c4f4c926
Mehedi Hasan <mhedih32@gmail.com>
2024-03-14 08:49:53 +0600

    new content added

--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import './Bottles.css'
+import { addToLS, getStoredCart } from "../../Utilities/localstorage";
 
 const Bottles = () => {
     const [bottles,setBottles]=useState([])
@@ -11,10 +12,29 @@ const Bottles = () => {
         .then(res => res.json())
         .then(data =>setBottles(data))
     },[])
+    // load cart from local storage
+    useEffect(()=>{
+        console.log('call the use effect',bottles.length)
+        if(bottles.length) {
+            const storedCard=getStoredCart()
+            console.log(storedCard,bottles)
+            const saveCart=[]
+            for(const id of storedCard){
+                console.log(id)
+                const bottle=bottles.find(bottle=>bottle.id === id)
+                if(bottle){
+                    saveCart.push(bottle)
+                }
+            }
+            console.log('saved card',saveCart)
+            setCart(saveCart)
+        }
+    },[bottles])
 
     const handleAddToCart = bottle=>{
         const newCart=[...cart,bottle]
         setCart(newCart)
+        addToLS(bottle.id)
     }
     return (
         <div>
